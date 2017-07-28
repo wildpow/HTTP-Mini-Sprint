163eb2a61e40893dddd53d6407c9f68c2e2cd87d
Sean Chen <skypemaster007@gmail.com>
2017-07-28 16:02:50 -0700

    Add initial files

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+const friends = [
+	{
+		name: 'Ben',
+		age: 30,
+		email: 'ben@lambdaschool.com',
+	},
+	{
+		name: 'Austen',
+		age: 45,
+		email: 'austen@lambdaschool.com',
+	},
+	{
+		name: 'Ryan',
+		age: 15,
+		email: 'ryan@lambdaschool.com',
+	},
+	{
+		name: 'Michelle',
+		age: 67,
+		email: 'michelle@gmail.com',
+	},
+];
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get('/friends', (req, res) => {
+	res.send(friends);
+});
+
+app.post('/new-friend', (req, res) => {
+	friends.push(req.body);
+	res.send(friends);
+});
+
+app.listen(5000, () => {
+	console.log('server listening on port 5000');
+});
