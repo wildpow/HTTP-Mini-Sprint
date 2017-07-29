0c1a58ea6867f85c7aa8f692adba73c5a091990c
Sean Chen <skypemaster007@gmail.com>
2017-07-28 19:03:30 -0700

    Add reducer code back in

--- http/src/reducers/index.js
+++ http/src/reducers/index.js
@@ -2,7 +2,12 @@ import { combineReducers } from 'redux';
 import { GET_FRIENDS } from '../actions';
 
 const friendsReducer = (friends = [], action) => {
-    // Add the code for this reducer
+    switch(action.type) {
+        case GET_FRIENDS:
+            return action.payload.data;
+        default:
+            return friends;
+    }
 };
 
 const rootReducer = combineReducers({
