cf0e160cf40d93b1aaf19318f1aee8baec897f3d
Sean Chen <skypemaster007@gmail.com>
2017-07-28 19:05:25 -0700

    Remove a console log

--- http/src/reducers/index.js
+++ http/src/reducers/index.js
@@ -4,7 +4,6 @@ import { GET_FRIENDS } from '../actions';
 const friendsReducer = (friends = [], action) => {
     switch(action.type) {
         case GET_FRIENDS:
-            console.log('get_friends action: ', action.payload);
             return action.payload.data;
         default:
             return friends;
